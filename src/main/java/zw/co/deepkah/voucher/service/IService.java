da175b9f338bf82c63b6eec7d158dde17e715415
Tonderai Ndangana <ndangana8@gmail.com>
2018-12-24 10:24:40 +0200

    Add default methods to manipulate multipart files

--- src/main/java/zw/co/deepkah/voucher/service/IService.java
+++ src/main/java/zw/co/deepkah/voucher/service/IService.java
@@ -1,7 +1,12 @@
 package zw.co.deepkah.voucher.service;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 public interface IService<T> {
 
@@ -14,6 +19,27 @@ public interface IService<T> {
         void removeById(String Id);
         void remove(T t);
         T save(T t);
+
+    default  void init(){}
+
+    default void store(MultipartFile file) throws Exception {
+        System.out.println(file.getName());
+    }
+
+    default Stream<Path> loadAll(){
+        Path path = null;
+      return (Stream<Path>) path;
+    }
+
+    default  Path load(String filename){
+        Path path = null;
+        return path;
+    }
+
+    default Resource loadAsResource(String filename){
+     Resource resource= null;
+     return resource;
+    }
 
 
 }
