7650d321a8951c5931e481b89477e7c27446abc9
Tonderai Ndangana <ndangana8@gmail.com>
2019-01-05 12:59:34 +0200

    Add sales document

---
+++ src/main/java/zw/co/deepkah/voucher/document/Sales.java
@@ -0,0 +1,17 @@
+package zw.co.deepkah.voucher.document;
+
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+
+@Data
+@Document(collection = "sales")
+public class Sales extends BaseId {
+
+    private String voucherSerialNumber;
+    private LocalDate saleDate;
+    private String soldBy;
+
+
+}
