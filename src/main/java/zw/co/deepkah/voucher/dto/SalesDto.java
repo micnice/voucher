948eaacaf8d740a38fb26d7c359f67dc973edea4
Tonderai Ndangana <ndangana8@gmail.com>
2019-01-05 12:59:52 +0200

    Add salesDto document

--- src/main/java/zw/co/deepkah/voucher/dto/SalesDto.java
+++ src/main/java/zw/co/deepkah/voucher/dto/SalesDto.java
@@ -1,4 +1,13 @@
 package zw.co.deepkah.voucher.dto;
 
+import lombok.Data;
+
+
+
+@Data
 public class SalesDto {
+
+    private String voucherSerialNumber;
+    private String saleDate;
+    private String soldBy;
 }
