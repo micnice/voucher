9da2b21ea89b62e08f876d0a1c9b07fd3c62ce4c
Tonderai Ndangana <ndangana8@gmail.com>
2019-01-05 13:00:23 +0200

    Add sales repository

--- src/main/java/zw/co/deepkah/voucher/repository/SalesRepository.java
+++ src/main/java/zw/co/deepkah/voucher/repository/SalesRepository.java
@@ -1,4 +1,9 @@
 package zw.co.deepkah.voucher.repository;
 
-public interface SalesRepository {
+import org.springframework.data.mongodb.repository.MongoRepository;
+import zw.co.deepkah.voucher.document.Sales;
+
+public interface SalesRepository extends MongoRepository<Sales,String> {
+
+    Sales findByVoucherSerialNumber(String voucherSerialNumber);
 }
