8ec0ea916a5fa0626595003e4fed45d0ae4bd192
Tonderai Ndangana <ndangana8@gmail.com>
2019-01-14 17:28:57 +0200

    Remove service for facility ,Add service to save service provider

--- src/main/java/zw/co/deepkah/voucher/resolver/ClaimMutationResolver.java
+++ src/main/java/zw/co/deepkah/voucher/resolver/ClaimMutationResolver.java
@@ -15,21 +15,20 @@ public class ClaimMutationResolver implements GraphQLMutationResolver {
 
     private ClaimService claimService;
     private VoucherTypeService voucherTypeService;
-    private FacilityService facilityService;
     private BeneficiaryIdentificationService beneficiaryIdentificationService;
     private SalesService salesService;
+    private ServiceProviderService serviceProviderService;
 
     public Claim createClaim(ClaimDto claimDto){
         Claim claim = new Claim();
         BeneficiaryIdentification beneficiaryIdentification = beneficiaryIdentificationService.getOne(claimDto.getBeneficiaryIdentification()).get();
              claim.setBeneficiaryIdentification(beneficiaryIdentification);
-
-             Facility facility = facilityService.getOne(claimDto.getFacility()).get();
-          claim.setFacility(facility);
-          VoucherType voucherType = voucherTypeService.getOne(claimDto.getVoucherType()).get();
-          claim.setVoucherType(voucherType);
-          Sales sales = salesService.getOne(claimDto.getSales()).get();
-          claim.setSales(sales);
+             ServiceProvider serviceProvider = serviceProviderService.getOne(claimDto.getServiceProvider()).get();
+             claim.setServiceProvider(serviceProvider);
+             VoucherType voucherType = voucherTypeService.getOne(claimDto.getVoucherType()).get();
+            claim.setVoucherType(voucherType);
+            Sales sales = salesService.getOne(claimDto.getSales()).get();
+            claim.setSales(sales);
           return claimService.save(claim);
 
 
@@ -38,16 +37,18 @@ public class ClaimMutationResolver implements GraphQLMutationResolver {
 
     public Claim updateClaim(ClaimDto claimDto, Optional<String> claimId){
         Claim claim = new Claim();
-
-            claim = claimService.getOne(claimId.get()).get();
-            claim.setBeneficiaryIdentification(new BeneficiaryIdentification(claimDto.getBeneficiaryIdentification()));
-
-            Facility facility = facilityService.getOne(claimDto.getFacility()).get();
-            claim.setFacility(facility);
+        claimId.ifPresent(s -> {
+            claim.equals(claimService.getOne(claimId.get()).get());
+            BeneficiaryIdentification beneficiaryIdentification = beneficiaryIdentificationService.getOne(claimDto.getBeneficiaryIdentification()).get();
+            claim.setBeneficiaryIdentification(beneficiaryIdentification);
+            ServiceProvider serviceProvider = serviceProviderService.getOne(claimDto.getServiceProvider()).get();
+            claim.setServiceProvider(serviceProvider);
             VoucherType voucherType = voucherTypeService.getOne(claimDto.getVoucherType()).get();
             claim.setVoucherType(voucherType);
             Sales sales = salesService.getOne(claimDto.getSales()).get();
             claim.setSales(sales);
+        });
+
 
         return claimService.save(claim);
     }
