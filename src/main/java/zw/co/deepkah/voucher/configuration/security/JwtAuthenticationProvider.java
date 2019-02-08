9db67c29a5332b8726dba5761c57bd9208ee8bd2
Tonderai Ndangana <ndangana8@gmail.com>
2019-02-08 02:12:23 +0200

    Provision of token expiry time and secret

--- src/main/java/zw/co/deepkah/voucher/configuration/security/JwtAuthenticationProvider.java
+++ src/main/java/zw/co/deepkah/voucher/configuration/security/JwtAuthenticationProvider.java
@@ -1,17 +1,22 @@
-package com.techprimers.security.jwtsecurity.security;
+package zw.co.deepkah.voucher.configuration.security;
 
-import com.techprimers.security.jwtsecurity.model.JwtAuthenticationToken;
-import com.techprimers.security.jwtsecurity.model.JwtUser;
-import com.techprimers.security.jwtsecurity.model.JwtUserDetails;
+
+import com.sun.security.auth.UserPrincipal;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.dao.AbstractUserDetailsAuthenticationProvider;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+import zw.co.deepkah.voucher.document.security.JwtAuthenticationToken;
+import zw.co.deepkah.voucher.document.security.User;
 
+import java.util.Date;
 import java.util.List;
 
 @Component
@@ -25,23 +30,36 @@ public class JwtAuthenticationProvider extends AbstractUserDetailsAuthentication
 
     }
 
+    public String generateToken(Authentication authentication) {
+
+        User userPrincipal = (User) authentication.getPrincipal();
+
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + 604800000);
+
+        return Jwts.builder()
+                .setSubject((userPrincipal.getId()))
+                .setIssuedAt(new Date())
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS512, "vms@20!8@#")
+                .compact();
+    }
+
     @Override
     protected UserDetails retrieveUser(String username, UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken) throws AuthenticationException {
 
         JwtAuthenticationToken jwtAuthenticationToken = (JwtAuthenticationToken) usernamePasswordAuthenticationToken;
         String token = jwtAuthenticationToken.getToken();
 
-        JwtUser jwtUser = validator.validate(token);
+        User user = validator.validate(token);
 
-        if (jwtUser == null) {
+        if (user == null) {
             throw new RuntimeException("JWT Token is incorrect");
         }
 
         List<GrantedAuthority> grantedAuthorities = AuthorityUtils
-                .commaSeparatedStringToAuthorityList(jwtUser.getRole());
-        return new JwtUserDetails(jwtUser.getUserName(), jwtUser.getId(),
-                token,
-                grantedAuthorities);
+                .commaSeparatedStringToAuthorityList(String.valueOf(user.getRolesSet()));
+        return new User( user.getId(),user.getUsername(),token,grantedAuthorities);
     }
 
     @Override
