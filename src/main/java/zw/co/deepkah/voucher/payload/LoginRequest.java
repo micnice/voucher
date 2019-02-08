e31daa72976ddd17a5c79e7ecab8a6dbe6739ea0
Tonderai Ndangana <ndangana8@gmail.com>
2019-02-08 02:15:19 +0200

    Create login request for rest controller

--- src/main/java/zw/co/deepkah/voucher/payload/LoginRequest.java
+++ src/main/java/zw/co/deepkah/voucher/payload/LoginRequest.java
@@ -1,10 +1,8 @@
-package com.example.polls.payload;
+package zw.co.deepkah.voucher.payload;
 
 import javax.validation.constraints.NotBlank;
 
-/**
- * Created by rajeevkumarsingh on 02/08/17.
- */
+
 public class LoginRequest {
     @NotBlank
     private String usernameOrEmail;
